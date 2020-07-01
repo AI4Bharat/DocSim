f802d75cb5c226c4b3dd207ded76a47090fcfaea
Gokul NC <gokulnc@ymail.com>
2020-07-01 14:21:45 +0530

    Support for European Transliteration

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Synthetically generate documents!
 
 - Atleast Python 3.7
 - `pip install -r dependencies.txt`
+- Place your fonts in the `fonts\` folder
+
+## Features
+
+- JSON template-based synthetic images and ground-truth generation
+- Support for filling using multiple-languages
+- Transliteration-based parallel multi-lingual parallel text
+- Support for QR and Bar code generation
--- docsim/utils/transliterator.py
+++ docsim/utils/transliterator.py
@@ -2,11 +2,30 @@ class Transliterator:
     def __init__(self, src_lang, dest_lang):
         if src_lang in IndicTransliterator.LANG2SCHEME and dest_lang in IndicTransliterator.LANG2SCHEME:
             self.transliterator = IndicTransliterator(src_lang, dest_lang)
+        elif src_lang in EuropeanTransliterator.LANGS and dest_lang in EuropeanTransliterator.LANGS:
+            self.transliterator = EuropeanTransliterator(src_lang, dest_lang)
         else:
             raise NotImplementedError
         
         self.transliterate = self.transliterator.transliterate
         self.reverse_transliterate = self.transliterator.reverse_transliterate
+
+
+import transliterate as euro_transliterate
+class EuropeanTransliterator:
+    LANGS = set(euro_transliterate.get_available_language_codes() + ['en'])
+    def __init__(self, src_lang, dest_lang):
+        self.src_lang = src_lang
+        self.dest_lang = dest_lang
+        
+    def transliterate(self, phrase):
+        en_phrase = phrase if self.src_lang == 'en' else euro_transliterate.translit(phrase, self.src_lang, reversed=True)
+        return euro_transliterate.translit(en_phrase, self.dest_lang)
+        
+    def reverse_transliterate(self, phrase):
+        en_phrase = phrase if self.dest_lang == 'en' else euro_transliterate.translit(phrase, self.dest_lang, reversed=True)
+        return euro_transliterate.translit(en_phrase, self.src_lang)
+
 
 from indic_transliteration import sanscript
 from indic_transliteration.sanscript import transliterate as indic_transliterate
