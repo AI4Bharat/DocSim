deb92831bc97f16324a39d39f92ae0b2838118b0
shubham <randiveshubham3@gmail.com>
2021-07-06 18:50:16 +0530

    remove command line argument

--- generate.py
+++ generate.py
@@ -3,8 +3,12 @@ from docsim.generator import Generator
 
 if __name__ == '__main__':
     # TODO: Use argparse
-    template_json = sys.argv[1]
-    samples = int(sys.argv[2]) if len(sys.argv) > 2 else 1
-    output_folder = sys.argv[3] if len(sys.argv) > 3 else None
-    
+    #template_json = sys.argv[1]
+    #samples = int(sys.argv[2]) if len(sys.argv) > 2 else 1
+    #output_folder = sys.argv[3] if len(sys.argv) > 3 else None
+
+    #TODO uncomment above code remove below code
+    template_json = "templates/PAN-HI/Latest/template.json"
+    samples= 10
+    output_folder = "output"
     Generator(template_json).generate(samples, output_folder)
